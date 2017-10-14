713c8fd74c823c71426f5c92259c33ba73837741
Sergey Antonov <asergey96@gmail.com>
2017-10-15 01:28:45 +0300

    added pre-filled name and email

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,9 +2,16 @@ class FeedbacksController < ApplicationController
   expose_decorated :feedback
   expose_decorated :feedbacks, -> { fetch_feedbacks }
 
+  def new
+    if current_user
+      feedback.name = current_user.full_name
+      feedback.email = current_user.email
+    end
+  end
+
   def create
     FeedbackMailer.feedback(feedback).deliver_now if feedback.save
-    respond_with(feedback)
+    respond_with(feedback, location: new_feedback_path)
   end
 
   private
