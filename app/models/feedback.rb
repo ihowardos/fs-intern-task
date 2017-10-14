d13013701b665f0874c282b9f10f867d62757056
Sergey Antonov <asergey96@gmail.com>
2017-10-14 22:28:26 +0300

    fix

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,11 +1,11 @@
 class Feedback < ApplicationRecord
   validates :name,
               presence: true,
-              length: { in 2..32 }
+              length: { in: 2..32 }
   validates :email,
               presence: true,
               length: { maximum: 64 },
-              format: { with: "\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z"}
+              format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, "invalid email format" }
   validates :text,
               presence: true,
               length: { maximum: 500 }
